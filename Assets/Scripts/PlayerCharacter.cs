aaea873b788e8e88a47eba39650d0fea9b33e66e
BagboBillbins <33336191+BagboBillbins@users.noreply.github.com>
2018-10-03 13:39:11 -0500

    Added code to physics of character

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour {
 
+    [SerializeField]
+    private Rigidbody2D rb2d;
+    [SerializeField]
+    private float speed = 5;
+    private float horzInput;
+    
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+    //Leave input to call as fast as possible
+	void Update ()
+    {
+        horzInput = Input.GetAxis("Horizontal");  
+        
 	}
+
+    //physics calls should be here so performance is optimized
+    void FixedUpdate()
+    {
+        rb2d.AddForce(Vector2.right * horzInput * speed);
+    }
 }
